88fa42db641cfd172eaf6e7f0fc0808f9ba8cd18
Aditya Shukla <128882999+Adi-tya-shukla@users.noreply.github.com>
2023-05-28 15:46:02 +0530

    Add files via upload
    With buttons for removing item and also updating value to 100 whenever we click on the update value

---
+++ update amount button-components/components/ExpenseItem.js
@@ -0,0 +1,33 @@
+import React,{ useState } from 'react';
+
+import ExpenseDate from './ExpenseDate';
+// import Card from './Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+const[amount, setAmount] = useState(props.amount);
+
+const clickHandler=()=>{
+  setAmount('100');
+}
+
+  return (
+    <div className='expense-item'>
+  <ExpenseDate date={props.date} />
+  <div className='expense-item__description'>
+    <h2>{props.title}</h2>
+    <div className='expense-item__Location'>{props.location}</div>
+    <div className='expense-item__price'>Rs.{amount}/-</div>
+  </div>
+  <button onClick={clickHandler}>ChangeAmount</button>
+  <button onClick={() => {
+    const parentElement = document.querySelector('.expense-item');
+    parentElement.remove();
+  }}>
+    Delete
+  </button>
+</div>
+  );
+}
+
+export default ExpenseItem;
