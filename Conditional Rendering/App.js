6688ce847a1d4cdf9fe02773fa4510277ed56bdd
Aditya Shukla <128882999+Adi-tya-shukla@users.noreply.github.com>
2023-05-31 14:09:34 +0530

    Add files via upload
    Conditional Rendering and  When the list has only a single Element, below that expense list show a message stating "Only single Expense here. Please add more..."

---
+++ Conditional Rendering/App.js
@@ -0,0 +1,60 @@
+import React,{useState} from 'react';
+
+import Expenses from './components/Expenses/Expense';
+import TotalAmount from './components/Expenses/TotalAmount';
+import NewExpense from './components/Expenses/NewExpenses/NewExpense';
+const dummy = [ {
+  id: 'e1',
+  title: 'Mall Shopping',
+  amount: 15090,
+  date: new Date(2021, 7, 14),
+},
+{ 
+  id: 'e2', 
+  title: 'New TV', 
+  amount: 7997.50, 
+  date: new Date(2021, 2, 12),
+},
+{
+  id: 'e3',
+  title: 'Food Night',
+  amount: 2982,
+  date: new Date(2022, 2, 28),
+},
+{
+  id: 'e4',
+  title: 'New Desk (Wooden)',
+  amount: 450,
+  date: new Date(2022, 5, 12),
+},
+{
+  id: 'e5',
+  title: 'Mobile',
+  amount: 5900,
+  date: new Date(2023, 5, 12),
+},]
+const App = () => {
+  const [expenses, setExpenses] = useState(dummy);
+
+  const onAddExpense = (expense) => {
+    setExpenses((prevExpenses) => [expense,...prevExpenses]);
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  return (
+    <div>
+      <h2><b><i><u>Expense Tracker</u></i></b></h2>
+    <h2 >Total Amount: Rs. <TotalAmount expenses={expenses} /></h2> 
+    <div><NewExpense onAddExpense ={onAddExpense}/></div>
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+export default App;
---
+++ Conditional Rendering/components/Expenses/ExpensesList.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import './ExpensesList.css';
+
+const ExpensesList = props => {
+    let expensesContent = props.items.map((expense) => (
+        <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+        />
+    ));
+
+    if (props.items.length === 0) {
+
+        return <p className="expenses-list__fallback">No expenses found.</p>;
+    }
+    else if (props.items.length === 1) {
+        return(
+        <>
+            <p className="expenses-list__fallback">Only single Expense here. Please add more..."</p>;
+            {expensesContent}
+        </>
+
+        )
+    }
+    return (
+        <ul className="expenses-list">
+            {expensesContent}
+        </ul>
+    )
+}
+
+export default ExpensesList;
