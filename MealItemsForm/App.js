4b02bd7982f1dd47ffc5a2112049db4a87d55aac
Aditya Shukla <128882999+Adi-tya-shukla@users.noreply.github.com>
2023-06-25 12:59:02 +0530

    Add files via upload

---
+++ MealItemsForm/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from './Components/Layout/Header'
+import Footer from './Components/Layout/Footer'
+import Meals from "./Components/Meals/Meals";
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="app-container">
+        <Header />
+        <main className="content">
+          <Meals />
+        </main>
+        <Footer />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
